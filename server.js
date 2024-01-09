01e2e5cee5e0d3a587a3d8e73fbd7df75ebacdb6
sai9173 <142866872+sai9173@users.noreply.github.com>
2024-01-09 10:41:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express();
+const cors= require("cors")
+const mongoose = require('mongoose');
+app.get('/',(req,res)=>{
+    res.send("Hello from server")
+}
+)
+const DB = async ()=>{
+    await mongoose.connect('mongodb+srv://pallaramkumar:Sai0251@cluster0.6s8kbq7.mongodb.net/?retryWrites=true&w=majority').then(()=>{console.log("connected to database...")}
+    ).catch(
+        (err)=>{console.log("An error occured while connecting to database..")}
+    )
+}
+DB();
+
+
+app.listen(8050,()=>{console.log("Server is running...")})
